997724996b5b27f981b79d298f9a2861977946f4
Jefferson souza <582jeffersons@gmail.com>
2023-12-12 22:01:42 +0000

    projeto-em-php commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php 
+    define('HOST','localhost');
+    define('USER','root');
+    define('PASSWORD','*Souza582');
+    define('BASE','cadastro');
+
+    $conn = new mysqli(HOST,USER,PASSWORD,BASE);
+    
+
+?>
---
+++ novo-usuario.php
@@ -0,0 +1,25 @@
+<h1>Novo usuário</h1>
+
+<form action="?page=salvar" method="POST">
+    <input type="hidden" name="acao" value="cadastrar">
+
+    <div class="mb-3">
+        <label for="#">Nome</label>
+        <input type="text" name="nome" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label for="#">E-mail</label>
+        <input type="text" name="email" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label for="#">Senha</label>
+        <input type="password" name="senha" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label for="#">Data de nascimento</label>
+        <input type="date" name="data_nasc" class="form-control">
+    </div>
+    <div class="mb-3">
+        <button type="submit" class="btn btn-primary">Enviar</button>
+    </div>
+</form>
